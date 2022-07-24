6e0b442838f2d2239960f80310d3be76a1fe45ce
Craig Watt <craig_watt@live.co.uk>
2022-07-24 12:17:57 +0100

    Pass handleSearch to Navbar

--- src/features/homepage/Home.js
+++ src/features/homepage/Home.js
@@ -4,11 +4,11 @@ import Navbar from '../../components/navbar/Navbar';
 const Home = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(e);
+        console.log(e.target.query.value);
     }
     return(
         <div>
-            <Navbar subreddits={[]} onSubmit={handleSubmit} />
+            <Navbar subreddits={[]} handleSubmit={handleSubmit} />
         </div>
     );
 }
